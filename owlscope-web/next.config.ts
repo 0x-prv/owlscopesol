eb52da939546055b3adf5a2ab6addd7be25c7c75
princeroveev-art <princeroveev@gmail.com>
2026-07-16 10:58:58 +0800

    Allow remote token logo image hosts

--- owlscope-web/next.config.ts
+++ owlscope-web/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "arweave.net",
+      },
+      {
+        protocol: "https",
+        hostname: "static.jup.ag",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
